a6d68578da59d3591e740788ba022387fbecca05
FrankBrongers <frank.brongers@gmail.com>
2021-01-15 14:26:28 +0100

    add in subsetloader

--- disentanglement.py
+++ disentanglement.py
@@ -2,46 +2,56 @@ import numpy as np
 import random
 import torch
 
+from torch.utils.data import SubsetRandomSampler
+
 
 class disentanglement_score:
-    def __init__(self, model, dataloader, z_dim, subset_fraction=.1, subset_size=None):
-        self.dataloader = dataloader
-        self.classes = dataloader.dataset.latents_classes
-        l = dataloader.len()
+    def __init__(self, model, dataset, args, z_dim, subset_fraction=.1, subset_size=None):
+        self.z_dim = args.z_dim
+        self.batch_size = args.batch_size
+        self.num_workers = args.num_workers
+        self.device = args.device
 
-        self.max_classes = dataloader.dataset.latents_classes[-1]
+        self.dataset = dataset
+        self.classes = dataset.latents_classes
+        self.max_classes = dataset.latents_classes[-1]
         self.count_classes = len(self.max_classes)
 
-        self.z_dim = z_dim
+        l = dataset.len()
 
         if not subset_size:
-            subset_size = numpy.round(l*subset_fraction)
+            subset_size = np.round(l*subset_fraction)
 
-        assert l >= subset_size
+        assert l >= subset_size, 'subset cannot be bigger than the dataset'
 
         subset = random.sample(range(0, l), subset_size)
-        latents = torch.zeros(subset_size, z_dim*2)
 
-        for c, idx in enumerate(subset):
-            input, _ = dataloader[idx]
-            z = self.model(input, no_dec=True).cpu().detach()
-            latents[c] = z
+        dataloader = create_subsetloader(subset)
+
+        latents = torch.zeros(sample_size, z_dim*2)
+        for c, input in enumerate(dataloader):
+            z = self.model(input.to(self.device), no_dec=True).cpu().detach()
+            latents[c:c+z.size(0)] = z
 
         self.emp_std = torch.std(latents, dim=1, unbiased=True, keepdim=True)
 
-    def fixed_factor_latents(model, fixed_factor_index=1, sample_size):
+    def fixed_factor_latents(self, model, fixed_factor_index=1, sample_size):
         fixed_factor = random.randrange(self.max_classes[fixed_factor_index]+1)
         sample_indices = numpy.random.choice(np.where(self.classes[:, fixed_factor_index] == fixed_factor)[0])
 
+        dataloader = create_subsetloader(sample_indices)
+
+        c = 0
         latents = torch.zeros(sample_size, z_dim*2)
-        for c, idx in enumerate(sample_indices):
-            input, _ = dataloader[idx]
-            z = self.model(input, no_dec=True).cpu().detach()
-            latents[c] = z
+        for input in enumerate(dataloader):
+            z = self.model(input.to(self.device), no_dec=True).cpu().detach()
+            c2 = c+z.size(0)
+            latents[c:c2] = z
+            c = c2
 
         return latents
 
-    def score(model, sample_size):
+    def score(self, model, sample_size):
         factors_indices = np.arange(self.count_classes)[1:]
 
         for idx in factors_indices:
@@ -50,7 +60,7 @@ class disentanglement_score:
             emp_var = self.empirical_variance(norm_latents, sample_size)
             vote = torch.argmin(input, dim=1)
 
-    def empirical_variance(latents, sample_size):
+    def empirical_variance(self, latents, sample_size):
         denom = (2*sample_size*(sample_size-1))
 
         sum_d = torch.zeros(self.z_dim)
@@ -61,3 +71,14 @@ class disentanglement_score:
             sum_d += d_i
 
         return sum_d/denom
+
+    def create_subsetloader(self, indices):
+        sampler = SubsetRandomSampler(sample_indices)
+        dataloader = DataLoader(self.dataset,
+                                batch_size=self.batch_size,
+                                sampler=sampler,
+                                num_workers=self.num_workers,
+                                pin_memory=True,
+                                drop_last=False)
+
+        return dataloader
